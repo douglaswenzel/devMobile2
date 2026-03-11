7cbfee34eac35792e02135253d761d6b217836c9
douglaswenzel <douglas.wenzel@hotmail.com>
2026-03-11 20:20:32 -0300

    fix(App.js): Limpeza do arquivo base e importação do componente de formulário

--- App.js
+++ App.js
@@ -1,55 +1,10 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ActivityIndicator, TextInput } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import FormAPI from './FormAPI';
 
 export default function App() {
-
-  const [cep, setCep] = useState([]);
-
-  const [loading, setLoading] = useState(false);
-
-  const buscaCep = async (X) => {
-    let url = `https://viacep.com.br/ws/${X}/json`;
-    setLoading(true);
-    await fetch(url)
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(data)
-        setCep(data);
-        console.log("-----" + data.ddd);
-      })
-      .catch(error => console.log(error));
-
-    setLoading(false);
-  };
-
-
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button
-        title=" Cep "
-        onPress={() => buscaCep('18080000')}
-      />
-
-      <TextInput
-        value={cep.logradouro}
-        onChangeText={text => setCep({ ...cep, logradouro: text})}
-        style={ {height: 40, borderColor: 'gray', borderWidth: 1} }
-      />
-
-      {loading && <ActivityIndicator size="large" />}
-
-      {cep != null && (
-        <View>
-          <Text> Rua: {cep.logradouro} </Text>
-          <Text> Bairro: {cep.bairro}</Text>
-          <Text> Cidade: {cep.cidade} </Text>
-          <Text> Estado: {cep.uf} </Text>
-        </View>)
-      }
-
+      <FormAPI />
     </View>
   );
 }

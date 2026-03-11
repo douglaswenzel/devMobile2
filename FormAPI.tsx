b4cde7a5bec95c2f667108f7a601dbcb2bc0c41f
douglaswenzel <douglas.wenzel@hotmail.com>
2026-03-11 20:26:09 -0300

    feat(formData): criação da manipulação dos estados por useeState em formato de array

--- FormAPI.tsx
+++ FormAPI.tsx
@@ -2,15 +2,23 @@ import * as React from 'react';
 import { TextInput } from 'react-native-paper';
 
 const FormAPI = () => {
-  const [text, setText] = React.useState('');
+  const [formData, setFormData] = React.useState({
+    cep: '',
+    logradouro: '', 
+    numero: '',
+    complemento: '', 
+    bairro: '',
+    localidade: '',
+    uf: ''
+  });
 
   return (
     <TextInput
       mode="outlined"
       label="CEP"
       placeholder="00000-000"
-      value={text}
-      onChangeText={setText}
+      value={formData.cep}
+      onChangeText={(v) => setFormData({...formData, cep: v})}
       right={<TextInput.Affix text="/8" />}
     />
   );

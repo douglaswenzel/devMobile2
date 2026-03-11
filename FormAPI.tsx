962f6b6ab5bb8b186d14fbabb6295741cec26bff
douglaswenzel <douglas.wenzel@hotmail.com>
2026-03-11 20:56:13 -0300

    feat(FormAPI): adicionar lista de estados brasileiros ao componente

--- FormAPI.tsx
+++ FormAPI.tsx
@@ -12,6 +12,12 @@ const FormAPI = () => {
     uf: ''
   });
 
+  const ESTADOS_BR = [
+    'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 
+    'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 
+    'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+  ];
+
   return (
     <TextInput
       mode="outlined"
